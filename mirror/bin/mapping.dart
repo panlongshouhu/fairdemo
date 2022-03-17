5ef921c2f8f7ec977fe5fa46e02518f44844c312
石强 <shiqiang2@tal.com>
2022-03-17 23:39:31 +0800

    mirror

---
+++ mirror/bin/mapping.dart
@@ -0,0 +1,3 @@
+class Mapping{
+  static Map<String,Function> map = Map();
+}
--- mirror/bin/mirror.dart
+++ mirror/bin/mirror.dart
@@ -1,43 +1,55 @@
-import 'dart:math';
 
+import 'dart:math';
 import 'dart:mirrors';
 
-void main(List<String> arguments) {
-  InstanceMirror classMirror = reflect(() =>
-      Random()) as InstanceMirror; //类名即是Type
+import 'mapping.dart';
+
+void main(List<String> arguments) async{
+  // InstanceMirror classMirror = reflect(() =>
+  //     Random()) as InstanceMirror; //类名即是Type
   // print(classMirror.reflectee.runtimeType);
-  Random random = Random();
+  // Random random = Random();
   // print(random.runtimeType);
 
   // ClassMirror c = reflectClass('Random');
   // InstanceMirror im = c.newInstance(const Symbol(''), ['MyAttributeValue']);
   // var o = im.reflectee;
-  String test = 'FormatException';
+  // String test = 'FormatException';
   LibraryMirror libraryMirror = currentMirrorSystem().findLibrary(
       new Symbol('dart.core'));
-  libraryMirror.declarations.forEach((key, value) {
+  libraryMirror.declarations.forEach((key, value) async {
+
     // print(value.toString());
     // DeclarationMirror classMirror = value;
     if (value is ClassMirror) {
       String name = MirrorSystem.getName(key);
-      if (test.isNotEmpty && test == name) {
-        // value.declarations.forEach((symbol, declarationMirror) {
-        //   print(MirrorSystem.getName(symbol));
-        // });
+      if (!name.startsWith('_')) {
+        value.declarations.forEach((symbol, declarationMirror) {
+          // print(MirrorSystem.getName(symbol));
+        });
 
         // This doesn't show me the constructor
         // print("+ Members");
         // value.instanceMembers.forEach((symbol, methodMirror) {
         //   print(MirrorSystem.getName(symbol));
         // });
+        // value.metadata.forEach((element) {
+        //   element.
+        // });
+        Function function = () => value.newInstance(Symbol(name), []);
+        Mapping.map[name] = function;
 
-        var instance = value.newInstance(Symbol.empty, []);
+        // var instance = value.newInstance(Symbol.empty, []);
         // print(instance);
         // value.instanceMembers.forEach((key, value) {
         //   print(value.constructorName);
         // });
       }
     }
+
+  });
+  Mapping.map.forEach((key, value) {
+    print("$key +${value.toString()}");
   });
   // print(libraryMirror.declarations);
 
